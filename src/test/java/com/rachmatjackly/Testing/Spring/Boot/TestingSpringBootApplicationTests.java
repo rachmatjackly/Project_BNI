9ee58e0c780b03fe37e33491ecfa066ccf14af6d
rachmat jackly <rachmatjackly@streamzinu.com>
2021-12-01 20:17:16 +0700

    initial file

---
+++ src/test/java/com/rachmatjackly/Testing/Spring/Boot/TestingSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rachmatjackly.Testing.Spring.Boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
